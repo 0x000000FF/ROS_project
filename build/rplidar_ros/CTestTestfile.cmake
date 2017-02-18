6f3fd9255b24cdf89147f9868c4626d45dd571d9
0x000000FF <qqqoptimus@gmail.com>
2017-02-18 16:46:53 +0800

    add slamtec rplidar A2

---
+++ build/rplidar_ros/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/qqq/ROS/jing/src/rplidar_ros
+# Build directory: /home/qqq/ROS/jing/build/rplidar_ros
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
